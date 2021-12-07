14b477f53ca07c686da0cc720ae03cbfbae4bbc9
AutomatedCommiter <alisra.tatakorn@gmail.com>
2021-12-07 12:58:45 +0100

    remove 1

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 
+USER="SomeRepo1"
+TOKEN="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"
+REPO="containers"
+REPOURL="https://${USER}:${TOKEN}@github.com/${USER}/${REPO}.git"
+ 
+TMP="testbox_gitbot"
+
+echo "CLOSE ${REPOURL} -> ${TMP}"
+git clone ${REPOURL} ${TMP}
+cd ${TMP}
 git config user.email alisra.tatakorn@gmail.com
 git config user.name AutomatedCommiter
 
+echo "OK"
